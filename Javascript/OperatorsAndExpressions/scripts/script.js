4ac094a7d3be9f712b349b1d0949f8e1ecb83c0d
Hristo <hmaleev@gmail.com>
2013-03-07 17:32:41 +0200

    Added tasks
    Added task 2, task 3, task 4

--- Javascript/OperatorsAndExpressions/scripts/script.js
+++ Javascript/OperatorsAndExpressions/scripts/script.js
@@ -1,3 +1,4 @@
+//task 1
 	var a =5;
 
 if (a%2==0) 
@@ -8,3 +9,33 @@ if (a%2==0)
 	{
 	jsConsole.writeLine("The result is: Odd")
 	};
+//task 2
+	var check = false;
+	var number=35;
+	if (number%5==0 && number%7==0) 
+		{
+			check =true;
+		}
+		if (check==true)
+		{
+			jsConsole.writeLine("This number can be divided by both 				5 and 7")
+		} 
+		else
+		{
+			jsConsole.writeLine("This number can't be divided by both			 5 and 7")
+		};
+//task 3
+var width=6;
+var height=7;
+var area;
+
+area=width*height;
+jsConsole.write("The area is: ");
+jsConsole.writeLine(area)
+//task 4
+var number=1732;
+var result;
+number = 1732/100;
+result=number%10
+jsConsole.writeLine(number);
+jsConsole.writeLine(result);
