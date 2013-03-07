5648d8892064d904cb07ac436d5b6f94751ad49f
Hristo <hmaleev@gmail.com>
2013-03-07 21:03:07 +0200

    Updated files
    Added list in index.html and functions in script.js

--- Javascript/OperatorsAndExpressions/scripts/script.js
+++ Javascript/OperatorsAndExpressions/scripts/script.js
@@ -1,3 +1,5 @@
+
+/*
 //task 1
 	var a =5;
 
@@ -39,3 +41,58 @@ number = 1732/100;
 result=number%10
 jsConsole.writeLine(number);
 jsConsole.writeLine(result);
+
+*/
+
+
+//task 1
+function task1 ()
+ {
+	var a =prompt ("Please choose a number to check");
+
+	if (a%2==0) 
+	{
+		alert("The result is: Even")
+	} 
+	else
+	{
+	alert("The result is: Odd")
+	};
+}
+
+//task 2
+function task2 ()
+{
+	var check = false;
+	var number=35;
+	if (number%5==0 && number%7==0) 
+		{
+			check =true;
+		}
+		if (check==true)
+		{
+			alert("This number can be divided by both 5 and 7")
+		} 
+		else
+		{
+			alert("This number can't be divided by both	 5 and 7")
+		};
+}
+		/*
+//task 3
+var width=6;
+var height=7;
+var area;
+
+area=width*height;
+alert("The area is: "+area);
+
+
+//task 4
+var number=1732;
+var result;
+number = 1732/100;
+result=number%10
+jsConsole.writeLine(number);
+jsConsole.writeLine(result);
+*/
