7e5919f5eed78453a4826dd6c3091888c83e0b14
Hristo <hmaleev@gmail.com>
2013-03-08 11:27:39 +0200

    Updated files
    Added two functions to script.js

--- Javascript/OperatorsAndExpressions/scripts/script.js
+++ Javascript/OperatorsAndExpressions/scripts/script.js
@@ -1,50 +1,3 @@
-
-/*
-//task 1
-	var a =5;
-
-if (a%2==0) 
-	{
-		jsConsole.writeLine("The result is: Even")
-	} 
-	else
-	{
-	jsConsole.writeLine("The result is: Odd")
-	};
-//task 2
-	var check = false;
-	var number=35;
-	if (number%5==0 && number%7==0) 
-		{
-			check =true;
-		}
-		if (check==true)
-		{
-			jsConsole.writeLine("This number can be divided by both 				5 and 7")
-		} 
-		else
-		{
-			jsConsole.writeLine("This number can't be divided by both			 5 and 7")
-		};
-//task 3
-var width=6;
-var height=7;
-var area;
-
-area=width*height;
-jsConsole.write("The area is: ");
-jsConsole.writeLine(area)
-//task 4
-var number=1732;
-var result;
-number = 1732/100;
-result=number%10
-jsConsole.writeLine(number);
-jsConsole.writeLine(result);
-
-*/
-
-
 //task 1
 function task1 ()
  {
@@ -78,21 +31,72 @@ function task2 ()
 			alert("This number can't be divided by both	 5 and 7")
 		};
 }
-		/*
+		
 //task 3
-var width=6;
-var height=7;
+function task3 ()
+{
+var width=prompt("choose the length of the rectangle:");
+var height=prompt("Choose the width of the rectangle");
 var area;
 
 area=width*height;
 alert("The area is: "+area);
-
+}
 
 //task 4
-var number=1732;
+function task4 ()
+{
+var number=1832;
 var result;
-number = 1732/100;
+number = number/100;
 result=number%10
-jsConsole.writeLine(number);
-jsConsole.writeLine(result);
-*/
+if (result%7>=0 && result%7<1)
+ {
+ 	alert("the third digit is 7")
+ }
+  else
+  	{
+  		alert("the third digit isn't 7")
+  	};
+}
+
+//task5
+function task5()
+{
+	var number=16;
+	var mask=1<<3;
+	var result =number&mask;
+	result=result>>3
+	
+	alert("The third bit in this number is: " +result);
+}
+
+//task6
+function task6()
+{
+	var xCoordinate=5;
+	var yCoordinate=2;
+	var radius =5;
+	var center=0;
+	var result;
+	var point;
+	var circle;
+	circle=Math.pow(radius,2)
+	point=Math.pow( (xCoordinate-center) ,2) + Math.pow( (xCoordinate-center) ,2);
+	if (point<circle) 
+		{
+			alert("the point is inside a circle")
+		}
+	else
+	 	{
+			alert("the point is not inside a circle")
+	 	};
+
+}
+//task7
+
+//task8
+function task8()
+{
+	
+}
