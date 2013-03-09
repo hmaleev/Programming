41bae503166e0960a8aca10cf8fd835456d89680
Hristo <hmaleev@gmail.com>
2013-03-09 11:21:05 +0200

    Created New Project
    Created new project - ConditionalOperators

---
+++ Javascript/ConditionalOperators/scripts/script.js
@@ -0,0 +1,50 @@
+//task 1
+function task1 ()
+{
+	
+}
+
+//task 2
+function task2 ()
+{
+
+}
+		
+//task 3
+function task3 ()
+{
+
+}
+
+//task 4
+function task4 ()
+{
+
+}
+
+//task5
+function task5()
+{
+
+}
+
+//task6
+function task6()
+{
+
+}
+
+
+
+function task7()
+{
+
+}
+
+
+
+//task8
+function task8()
+{
+
+}
