41643ff85223c3289c9fc5afaa58db6446db865a
Hristo <hmaleev@gmail.com>
2013-03-14 22:38:07 +0200

    Updated ConditionalOperators
    Added function and various small fixes;

--- Javascript/ConditionalOperators/scripts/script.js
+++ Javascript/ConditionalOperators/scripts/script.js
@@ -1,16 +1,17 @@
 //task 1
 function task1 ()
 {
-	var a =42;
-	var b=10;
-	var c;
-	if (a>b)
+	var firstNumber =42;
+	var secondNumber=10;
+	var exchange;
+	if (firstNumber>secondNumber)
 		{
-			c=a;
-			a=b;
-			b=c;
-			alert(a);
-			alert(b);
+			exchange=firstNumber;
+			firstNumber=secondNumber;
+			secondNumber=exchange;
+			
+			alert("After the exchange the first number is: "+firstNumber);
+			alert("After the exchange the second number is: "+secondNumber);
 		}
 
 }
@@ -18,19 +19,19 @@ function task1 ()
 //task 2
 function task2 ()
 {
-	var a=-5;
-	var b=2;
-	var c=5;
+	var firstNumber=parseInt(prompt("enter first number:"));
+	var secondNumber=parseInt(prompt("enter second number:"));
+	var thirdNumber=parseInt(prompt("enter third number:"));
 	var count=0;
-	if (a<0)
+	if (firstNumber<0)
 	{
 		count++;
 	} 
-	if (b<0)
+	if (secondNumber<0)
 	{
 		count++;
 	} 
-	if (c<0)
+	if (thirdNumber<0)
 	{
 		count++;
 	} 
@@ -49,33 +50,33 @@ function task2 ()
 //task 3
 function task3 ()
 {
-	var a=prompt("a");
-	var b=prompt("b");
-	var c=prompt("c");
+	var firstNumber=parseInt(prompt("enter first number:"));
+	var secondNumber=parseInt(prompt("enter second number:"));
+	var thirdNumber=parseInt(prompt("enter third number:"));
 	var count=0;
-	var max=a;
+	var max=firstNumber;
 //	alert(max);
 	
-	if (a<b)
+	if (firstNumber<secondNumber)
 	{
-		if (b<c)
+		if (secondNumber<thirdNumber)
 		{
-			max=c;
+			max=thirdNumber;
 		} 
 		else
 		{
-			max=b;
+			max=secondNumber;
 		}
 	}
 	else
 	{
-		if (a>c)
+		if (firstNumber>thirdNumber)
 		{
-			max=a;
+			max=firstNumber;
 		}
 		else
 		{
-		max=c;
+		max=thirdNumber;
 		};
 	}
 	alert("max: "+max);
@@ -88,52 +89,51 @@ function task3 ()
 //task 4
 function task4 ()
 {
-	var a=prompt("a");
-	var b=prompt("b");
-	var c=prompt("c");
+	var firstNumber=parseInt(prompt("enter first number:"));
+	var secondNumber=parseInt(prompt("enter second number:"));
+	var thirdNumber=parseInt(prompt("enter third number:"));
 	var exchange;
 	var count=0;
-	var max=a;
+	var max=firstNumber;
 //	alert(max);
 	
-	if (a<b)
+	if (firstNumber<secondNumber)
 	{
-		if (b<c)
+		if (secondNumber<thirdNumber)
 		{
-			exchange=a;
-			a=c;
-			c=exchange;
+			exchange=firstNumber;
+			firstNumber=thirdNumber;
+			thirdNumber=exchange;
 
 		} 
 		else
 		{
-			exchange=a;
-			a=b;
-			b=exchange;
-			if (b<c) 
+			exchange=firstNumber;
+			firstNumber=secondNumber;
+			secondNumber=exchange;
+			if (secondNumber<thirdNumber) 
 			{
-				exchange=b;
-				b=c;
-				c=exchange;
+				exchange=secondNumber;
+				secondNumber=thirdNumber;
+				thirdNumber=exchange;
 			}
 		}
 	}
 	else
 	{
-		if (b<c)
+		if (secondNumber<thirdNumber)
 		{
-			if (a>c)
+			if (firstNumber>thirdNumber)
 			{
-				exchange=b;
-				b=c;
-				c=exchange;
+				exchange=secondNumber;
+				secondNumber=thirdNumber;
+				thirdNumber=exchange;
 			} 
-			else
-			{};
+			
 		}
 
 	}
-	alert("Descending order: "+a+" "+b+" "+c);
+	alert("Descending order: "+firstNumber+" "+secondNumber+" "+thirdNumber);
 }
 
 //task5
@@ -179,6 +179,63 @@ function task6()
 function task7()
 {
 
+    var max=0;      
+    var firstNumber = parseInt( prompt("Please enter the first number:"));
+    var secondNumber = parseInt( prompt("Please enter the second number:"));
+ 	var	thirdNumber = parseInt( prompt("Please enter the thied number:"));
+ 	var	fourthNumber = parseInt( prompt("Please enter the fourth number:"));
+  	var	fifthNumber = parseInt( prompt("Please enter the fifth number:"));
+
+        if (firstNumber != secondNumber | firstNumber != thirdNumber)
+        {
+            if (firstNumber > secondNumber)
+            {
+                if (firstNumber > thirdNumber)
+                {
+                    max = firstNumber;
+                }
+                else
+                {
+                    max = thirdNumber;
+                }
+            }
+            else
+            {
+                if (secondNumber > thirdNumber)
+                {
+                    max = secondNumber;
+                }
+                else
+                {
+                    max = thirdNumber;
+                }
+            }
+        }
+       
+
+            if (fourthNumber > fifthNumber)
+            {
+                if (max > fourthNumber)
+                {
+                    alert("The biggest number is: "+ max);
+                }
+                else
+                {
+                   alert("The biggest number is: "+ fourthNUmber);
+                }
+            }
+            else
+            {
+                if (max > fifthNumber)
+                {
+                   alert("The biggest number is: " +max);
+                }
+                else
+                {
+                    alert("The biggest number is: " +fifthNumber);
+                }
+            }
+
 }
 
 
@@ -186,5 +243,103 @@ function task7()
 //task8
 function task8()
 {
+var input =parseInt(prompt("number:"));
+var res;
+var output="";
 
+res = parseInt(input/100);
+if (res>0) 
+{
+	switch (res)
+	{
+		//case 0: output ="Zero"; break;
+		case 1: output ="One Hundred"; break;
+		case 2: output ="Two Hundred"; break;
+		case 3: output ="Three Hundred"; break;
+		case 4: output ="Four Hundred"; break;
+		case 5: output ="Five Hundred"; break;
+		case 6: output ="Six Hundred"; break;
+		case 7: output ="Seven Hundred"; break;
+		case 8: output ="Eight Hundred"; break;
+		case 9: output ="Nine Hundred"; break;
+		default: alert("Out of Range"); break;
+	}
+};
+
+res = parseInt((input/10)%10);
+if (res!=0 && res!=1)
+{
+	switch (res)
+	{
+		
+
+		//case 1: output ="One Hundred"; break;
+		case 2: output +=" Twenty"; break;
+		case 3: output +=" Thirty"; break;
+		case 4: output +=" Fourty"; break;
+		case 5: output +=" Fifty"; break;
+		case 6: output +=" Sixty"; break;
+		case 7: output +=" Seventy"; break;
+		case 8: output +=" Eighty "; break;
+		case 9: output +=" Ninety"; break;
+		//default: alert("Out of Range"); break;
+	}
+		switch (input%10)
+	{
+		
+
+		case 1: output ="One"; break;
+		case 2: output +=" Two"; break;
+		case 3: output +=" Three"; break;
+		case 4: output +=" Four"; break;
+		case 5: output +=" Five"; break;
+		case 6: output +=" Six"; break;
+		case 7: output +=" Seven"; break;
+		case 8: output +=" Eight "; break;
+		case 9: output +=" Nine"; break;
+		//default: alert("Out of Range"); break;
+	}
+}
+
+
+else
+{
+	if (res==0) 
+		{	res=parseInt(input%10)
+			switch (res)
+			{
+				//case 0: output ="Zero"; break;
+				//case 1: output ="One Hundred"; break;
+				case 2: output +=" Two"; break;
+				case 3: output +=" Three"; break;
+				case 4: output +=" Four"; break;
+				case 5: output +=" Five"; break;
+				case 6: output +=" Six"; break;
+				case 7: output +=" Seven"; break;
+				case 8: output +=" Eight "; break;
+				case 9: output +=" Nine"; break;
+				//default: alert("Out of Range"); break;
+			}
+		}
+		
+	if (res==1) 
+	{
+		res=parseInt(input%10)
+			switch (res)
+			{
+				case 0: output +=" Ten"; break;
+				case 1: output +=" Eleven"; break;
+				case 2: output +=" Twelve"; break;
+				case 3: output +=" Thirteen"; break;
+				case 4: output +=" Fourteen"; break;
+				case 5: output +=" Fifteen"; break;
+				case 6: output +=" Sixteen"; break;
+				case 7: output +=" Seventeen"; break;
+				case 8: output +=" Eighteen "; break;
+				case 9: output +=" Nineteen"; break;
+				//default: alert("Out of Range"); break;
+			}
+	}
+} 
+	alert(output)
 }
