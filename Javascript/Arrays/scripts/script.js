3890e8e8c1f3eaecbbd548f465a34f6a738fa7ee
Hristo <hmaleev@gmail.com>
2013-03-21 21:09:48 +0200

    Created New Project - Arrays

---
+++ Javascript/Arrays/scripts/script.js
@@ -0,0 +1,42 @@
+//task 1
+function Task1 ()
+{
+
+}
+
+//task 2
+function Task2 ()
+{
+	
+}
+		
+//task 3
+function Task3 ()
+{
+
+}
+
+//task 4
+function Task4 ()
+{
+
+}
+
+//task 5
+function Task5 ()
+{
+
+}
+
+//task 6
+function Task6 ()
+{
+
+}
+
+//task 7
+function Task7 ()
+{
+
+}
+
