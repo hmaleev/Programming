3112f96d25dfaa8392b5b8a15b65dc4290f28d45
Hristo <hmaleev@gmail.com>
2013-03-23 10:15:43 +0200

    Created new project - Functions

---
+++ Javascript/Functions/scripts/script.js
@@ -0,0 +1,46 @@
+//task 1
+function Task1 ()
+{
+	document.getElementById("task1").innerHTML="";
+
+
+}
+function Task2 ()
+{
+	document.getElementById("task2").innerHTML="";
+
+}
+		
+//task 3
+function Task3 ()
+{
+
+}
+
+//task 4
+function Task4 ()
+{
+
+}
+
+//task 5
+function Task5 ()
+{
+
+}
+
+//task 6
+function Task6 ()
+{
+
+}
+
+
+
+
+//task 7
+function Task7 ()
+{
+
+}
+
