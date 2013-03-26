60029a53c4eac9714bc44e47919f80c6ae0ebae1
Hristo <hmaleev@gmail.com>
2013-03-26 19:52:01 +0200

    Updated project Functions
    Updated functions 3 to 5, added new function 6

--- Javascript/Functions/scripts/script.js
+++ Javascript/Functions/scripts/script.js
@@ -27,18 +27,20 @@ function Task1 ()
 	}
 
 }
+
+//task2
 function Task2 ()
 {
 	document.getElementById("task2").innerHTML;
 	var number =4205;
 	var arr1 = new Array();
 	var result;
-	var j=0;
+	
 
-	for (var i = number; i > 1; i/=10)
+	for (var i = number, j=0; i > 1; i/=10)
 	{
 
-		arr1[j]=parseInt(i%10);	
+		arr1[j]=parseInt(i%10);
 		j++;
 	};
 
@@ -47,35 +49,101 @@ function Task2 ()
 	document.getElementById("task2").innerHTML=result;
 }
 		
+
 //task 3
 function Task3 ()
 {
-var text = "Tomorrow will be the exam . The exam will begin at 10";
-var word ="exam";
-//var arr1 = new Array();
-arr1 = text.split(" " ||".");
-	document.getElementById("task3").innerHTML=arr1;
+	var inputText = document.getElementById("inputText").value;
+	var inputWord =document.getElementById("inputWord").value;
+
+	searchForWord(inputText,inputWord,"no")
+
+	function searchForWord (inputText,inputWord,type)
+	{
+		var text =inputText;
+		var word=inputWord;
+		var arr1 = new Array();
+
+		switch(type)
+		{
+			case "yes":
+				{
+					var count =0;
+					arr1=text.split(" ");
+					for (var i = 0; i < arr1.length; i++)
+					{	
+						if (arr1[i]==word)
+						{
+							count++;
+						}
+					};
+					document.getElementById("task3").innerHTML+=count;
+					break;
+				}
+			case "no":
+				{
+					var count =0;
+					text=text.toLowerCase();
+					arr1=text.split(" ");
+					for (var i = 0; i < arr1.length; i++)
+					{	
+						if (arr1[i]==word)
+						{
+							count++;
+						}
+					};
+					document.getElementById("task3").innerHTML+=count;
+					break;
+				}
+		}
+	}
+
 }
 
 //task 4
 function Task4 ()
 {
-
+	var webPage=document.getElementsByTagName("div");
+	var count=webPage.length;
+	document.getElementById("task4").innerHTML=count;
 }
+
 
 //task 5
 function Task5 ()
 {
+	var inputText = document.getElementById("inputNumber").value;
+	var number=document.getElementById("Number2").value;
+
+	var arr1=inputText.split(" "||",");
+
+
+	var count=0;
+		for (var i = 0; i <=arr1.length; i++)
+		{
+				if (arr1[i]==number)
+				{
+					count++;
+				};
+		};
+	document.getElementById("task5").innerHTML=count;
 
 }
+
+
 
 //task 6
 function Task6 ()
 {
+var isBigger =false;
+var arr1=new Array(1,1,3,1,3,6,6,6,7);
 
+if (arr1[2]>arr1[1] && arr1[2]>arr1[3])
+{
+	isBigger=true;
+document.getElementById("task6").innerHTML=isBigger;
+};
 }
-
-
 
 
 //task 7
