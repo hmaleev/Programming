b441109059a65d15a7e92c814e6d118ed25a5691
Hristo <hmaleev@gmail.com>
2013-03-27 10:34:20 +0200

    Added new project - Objects

---
+++ Javascript/Objects/scripts/script.js
@@ -0,0 +1,39 @@
+//task 1
+function Task1 ()
+{
+
+}
+
+//task2
+function Task2 ()
+{
+	
+}
+		
+
+//task 3
+function Task3 ()
+{
+
+}
+
+//task 4
+function Task4 ()
+{
+
+}
+
+
+//task 5
+function Task5 ()
+{
+
+}
+
+
+
+//task 6
+function Task6 ()
+{
+
+}
