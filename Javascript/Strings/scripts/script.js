885f2e0759119ff395e84f9e832f5d2002735cf8
Hristo <hmaleev@gmail.com>
2013-03-30 14:00:25 +0200

    Updated pr. Strings
    Added new functions

--- Javascript/Strings/scripts/script.js
+++ Javascript/Strings/scripts/script.js
@@ -1,7 +1,7 @@
 //task 1
 function Task1 ()
 {
-var str="Levski";
+var str="Proba";
 var res;
 var array = new Array();
 var newarr = new Array();
@@ -10,9 +10,13 @@ for (var i = 0, j = array.length-1; i < array.length; i++,j--)
 {
     newarr [i] = array[j];
 }
-res = newarr.toString();
-document.getElementById("task1").innerHTML = res;
-
+res = newarr.toString(newarr);
+//document.getElementById("task1").innerHTML = res;
+for (var i = 0; i < res.length; i++) {
+    if (i % 2 === 0) {
+        document.getElementById("task1").innerHTML += res[i];
+    }
+}
 }
 
 //task2
@@ -48,14 +52,77 @@ function Task2 ()
 function Task3 ()
 {
     var text = " We are living in an yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.";
+    var array = new Array();
+    var test = "in"
+    var count = 0;
+    var subString
+    array = text.split(" ");
+    for (var i = 0; i < array.length; i++) {
+        subString = array[i].toString();
+        if ( subString.contains(test))
+        {
+            count++;
+        }
+        if (subString === test) {
+            count++;
 
+        }
+
+    }
+
+    //document.getElementById("task3").innerHTML += array;
+    document.getElementById("task3").innerHTML += count;
 
 }
 
 //task 4
 function Task4 ()
 {
-	
+    var text = "We are <mixcase>living</mixcase> in a <upcase>yellow submarine</upcase>. We <mixcase>don't</mixcase> have <lowcase>ANYthing</lowcase> else.";
+
+    var upCaseBegining = "<upcase>";
+    var upCaseEnd = "</upcase>"
+    var lowCaseBegining = "<lowcase>";
+    var lowCaseEnd = "</lowcase>"
+    var isLowCase = false;
+    var array = new Array();
+    var isUpCase = false;
+    array = text.split(" ");
+    var subString;
+    for (var i = 0; i < array.length; i++) {
+        subString = array[i];
+        //--------upper casing-----------
+        if (subString.contains(upCaseBegining)) {
+            isUpCase = true;
+            //subString.contains("</upcase>");
+
+        }
+        if (isUpCase === true) {
+            array[i] = subString.toUpperCase();
+
+
+        }
+        if (subString.contains(upCaseEnd)) {
+            isUpCase = false;
+        }
+        //-----lower casing----------
+        if (subString.contains(lowCaseBegining)) {
+            isLowCase = true;
+            //subString.contains("</upcase>");
+
+        }
+        if (isLowCase === true) {
+            array[i] = subString.toLowerCase();
+
+
+        }
+        if (subString.contains(lowCaseEnd)) {
+            isLowCase = false;
+        }
+        //-----mix casing------------
+
+    }
+    document.getElementById("task4").innerHTML += array;
 }
 
 
@@ -78,5 +145,44 @@ function Task6 ()
 function Task7 ()
 {
 
-	
+    var text = "http://www.gong.bg/forum/index.php";
+    var protocol = "";
+    var server="";
+    var resource="";
+    var isProtocol = true;
+    var isServer = false;
+    var isResource = false
+
+    var length = text.length;
+    for (var i = 0; i < length; i++)
+    {
+        //--------protocol------------
+        if (isProtocol === true && text[i] !== ":") {
+            protocol += text[i];
+        }
+        else {
+            isProtocol = false;
+        }
+        if (isProtocol === false && text[i]==="w" )
+        {
+            isServer=true;
+            
+        }
+        //-------------server---------
+        if (isServer===true && text[i]!=="/") {
+            server+=text[i];
+        }
+        else {
+            isServer = false;
+        }
+        //-----resource-----------
+        if (isServer === false && isProtocol === false )
+        {
+            resource += text[i];
+        }
+    }
+    resource = resource.replace('://', "");
+    document.getElementById("task7").innerHTML += protocol + "</br>";
+    document.getElementById("task7").innerHTML += server + "</br>";
+    document.getElementById("task7").innerHTML += resource;
 }
