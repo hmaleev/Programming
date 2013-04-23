1135827f15f51fe052dd2e1501ddc48a18f1bf54
Hristo <hmaleev@gmail.com>
2013-04-24 00:01:50 +0300

    New Project
    Added the game Cows & Bulls written in JavaScript

---
+++ Javascript/CowsAndBulls/scripts/history.js
@@ -0,0 +1,5 @@
+function History() {
+    var randomNumber = document.getElementById("task2").innerHTML;
+    var playerNumber = document.getElementById("inputNumber").value;
+    document.getElementById("history").innerHTML = playerNumber;
+}
---
+++ Javascript/CowsAndBulls/scripts/script.js
@@ -0,0 +1,76 @@
+//task 1
+
+function CompareNumbers() {
+
+    var playerNumber = document.getElementById("inputNumber").value;
+    var playerNumberAsString = playerNumber.toString();
+    var i;
+    var j;
+    var bullCount = 0;
+    var cowCount = 0;
+    var randomNumber = document.getElementById("task2").innerHTML;
+    var randomNumberAsString = randomNumber.toString();
+    for (i = 0; i < playerNumber.length; i++) {
+        for (j = 0; j < playerNumber.length; j++) {
+
+            if (playerNumber.charAt(i) === randomNumberAsString.charAt(j) && i === j) {
+                bullCount++;
+                break;
+            }
+            if (playerNumber.charAt(i) === randomNumberAsString.charAt(j) && i !== j) {
+
+                cowCount++
+                break;
+            };
+        };
+    };
+    document.getElementById("task1").innerHTML = "Cows: " + cowCount + " Bulls: " + bullCount;
+}
+
+function GenerateRandomNumber() {
+
+    var randomNumber;
+    var lengthOfNumber = 1;
+    for (;;) {
+        randomNumber = Math.floor(Math.random() * 10000)
+        lengthOfNumber = randomNumber.toString().length;
+        if (lengthOfNumber === 4) {
+            var isRandomNumber = CheckForUniqueDigits(randomNumber);
+
+            break;
+        };
+
+    };
+    if (!isRandomNumber) {
+        GenerateRandomNumber();
+    } else {
+        document.getElementById("task2").innerHTML = randomNumber;
+    }
+}
+
+function CheckForUniqueDigits(number2) {
+
+    var number = number2;
+    //    var number = document.getElementById("task2").innerHTML;
+    var isUnique = true;
+    var i;
+    var j;
+    var lengthOfNumber = 1;
+    var numberAsString = number.toString();
+    for (i = 0; i < numberAsString.length - 1; i++) {
+        if (!isUnique) {
+            break;
+        };
+        for (j = i + 1; j <= numberAsString.length - 1; j++) {
+
+            if (numberAsString.charAt(i) !== numberAsString.charAt(j)) {
+                isUnique = true;
+            } else {
+                isUnique = false
+                break;
+            };
+        };
+    };
+    //  document.getElementById("task3").innerHTML = isUnique + " ";
+    return isUnique;
+};
