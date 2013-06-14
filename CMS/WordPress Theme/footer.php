89b556261fa827dfc89f2dc81b741ba90d564420
Hristo <hmaleev@gmail.com>
2013-06-15 00:19:29 +0300

    updated wordpress theme
    widgetized footer

--- CMS/WordPress Theme/footer.php
+++ CMS/WordPress Theme/footer.php
@@ -4,36 +4,53 @@
 		<section id="footer-area">
 
 			<section id="footer-outer-block">
+			<?php //get_sidebar( 'footer' ); ?>
 					<aside class="footer-segment">
+					<?php 
+						if(!dynamic_sidebar('footer-sidebar-1')) :
+					?>
 							<h4>Friends</h4>
 								<ul>
 									<li><a href="#">one linkylink</a></li>
 									<li><a href="#">two linkylinks</a></li>
 									<li><a href="#">three linkylinks</a></li>
 								</ul>
+					<?php endif;?>
 					</aside><!-- end of #first footer segment -->
 
 					<aside class="footer-segment">
+					<?php 
+						if(!dynamic_sidebar('footer-sidebar-2')) :
+					?>
 							<h4>Awesome Stuff</h4>
 								<ul>
 									<li><a href="#">one linkylink</a></li>
 									<li><a href="#">two linkylinks</a></li>
 									<li><a href="#">three linkylinks</a></li>
 								</ul>
+						<?php endif; ?>
 					</aside><!-- end of #second footer segment -->
 
 					<aside class="footer-segment">
+					<?php 
+						if(!dynamic_sidebar('footer-sidebar-3')) :
+					?>
 							<h4>Coolness</h4>
 								<ul>
 									<li><a href="#">one linkylink</a></li>
 									<li><a href="#">two linkylinks</a></li>
 									<li><a href="#">three linkylinks</a></li>
 								</ul>
+								<?php endif; ?>
 					</aside><!-- end of #third footer segment -->
 					
 					<aside class="footer-segment">
+					<?php 
+						if(!dynamic_sidebar('footer-sidebar-4')) :
+					?>
 							<h4>Blahdyblah</h4>
 								<p>&copy; 2010 <a href="#">yoursite.com</a> Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
+								<?php endif; ?>
 					</aside><!-- end of #fourth footer segment -->
 
 			</section><!-- end of footer-outer-block -->
--- CMS/WordPress Theme/functions.php
+++ CMS/WordPress Theme/functions.php
@@ -9,7 +9,8 @@ $sidebar_args = array(
 		'after_title'   => '</h3>' 
 		);
 
-$sidebar2_args = array(
+
+		$sidebar2_args = array(
 		'id' => 'right-sidebar-2',
 		'name' => 'Main Sidebar-2',
 		'before_widget' => '<ul><li>',
@@ -17,8 +18,50 @@ $sidebar2_args = array(
 		'before_title'  => '<h3>',
 		'after_title'   => '</h3>' 
 		);
-register_sidebar($sidebar_args);
 register_sidebar($sidebar2_args);
 
+register_sidebar($sidebar_args);
+
+$footerSidebar1 = array(
+		'id' => 'footer-sidebar-1',
+		'name' => 'Main Sidebar-1',
+		'before_widget' => '<ul ><li>',
+		'after_widget'  => '</li></ul>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>' 
+		);
+$footerSidebar2 = array(
+		'id' => 'footer-sidebar-2',
+		'name' => 'Main Sidebar-2',
+		'before_widget' => '<ul><li>',
+		'after_widget'  => '</li></ul>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>' 
+		);
+		
+
+register_sidebar($footerSidebar1);
+register_sidebar($footerSidebar2);
+
+$footerSidebar1 = array(
+		'id' => 'footer-sidebar-3',
+		'name' => 'Main Sidebar-3',
+		'before_widget' => '<ul ><li>',
+		'after_widget'  => '</li></ul>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>' 
+		);
+$footerSidebar2 = array(
+		'id' => 'footer-sidebar-4',
+		'name' => 'Main Sidebar-4',
+		'before_widget' => '<ul><li>',
+		'after_widget'  => '</li></ul>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>' 
+		);
+		
+
+register_sidebar($footerSidebar3);
+register_sidebar($footerSidebar4);
 register_nav_menu ('top-side-menu','This is top side menu');
 ?>
