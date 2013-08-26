704e3e07631e4d7bc40d3eea9c9ae791f86c64e2
Hristo <hmaleev@gmail.com>
2013-08-26 15:40:04 +0300

    added new projects
    added initial release of the navigation app

---
+++ Win8 Apps with HTML, CSS, JS/NavigationApp/NavigationApp/pages/home/home.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+
+            //window.gotocontacts = function gotocontacts(e) {
+            //    e.preventDefault();
+            //    WinJS.Navigation.navigate('/pages/contacts/contacts.html')
+            //}
+        }
+    });
+})();
+
+function gotocreation(e) {
+    e.preventDefault();
+    WinJS.Navigation.navigate('/pages/creation/creation.html');
+}
