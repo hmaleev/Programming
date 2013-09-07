3351b58b83cc62f50d557ad020d2921a6793d287
Hristo <hmaleev@gmail.com>
2013-09-07 22:19:49 +0300

    Added new project - Controls

---
+++ XAML/Controls/ButtonHW/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonHW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        uint count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            count++;
+            counter.Content = count;
+        }
+    }
+}
---
+++ XAML/Controls/checkedItems/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace checkedItems
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onChecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox item = sender as CheckBox;
+            //TextBoxContent.AppendText(" " + item.Content + " ");
+            TextBoxContent.Text +=(" " + item.Content + " ");
+        }
+
+        private void onUnchecked(object sender, RoutedEventArgs e)
+        {
+
+            CheckBox item = sender as CheckBox;
+            string text = TextBoxContent.Text;
+            text = text.Replace(item.Content.ToString(), string.Empty);
+            Console.WriteLine(text);
+            TextBoxContent.Text = text;
+        }
+    }
+}
