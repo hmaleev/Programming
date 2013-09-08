bd745d869099ed8b17d0d78d777acc4d8edc0f3d
Hristo <hmaleev@gmail.com>
2013-09-08 23:21:26 +0300

    Updated DataBinding

---
+++ XAML/DataBinding/TextBlockHW/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextBlockHW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onTextChange(object sender, TextChangedEventArgs e)
+        {
+            TextBox input = sender as TextBox;
+            TxtBlock.Text = input.Text;
+        }
+
+        private void onSelectedForegroundComboBox(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem color = sender as ComboBoxItem;
+            string foregroundColor = color.Content.ToString();
+            if (foregroundColor =="Green")
+	        {
+                TxtBlock.Foreground = Brushes.Green;
+	        }
+            else  if (foregroundColor =="Blue")
+            {
+                TxtBlock.Foreground = Brushes.Blue;
+            }
+            else if (foregroundColor == "Red")
+            {
+                TxtBlock.Foreground = Brushes.Red;
+            }
+
+        }
+
+        private void onSelectedBackgroundComboBox(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem color = sender as ComboBoxItem;
+            string colors = color.Content.ToString();
+            if (colors == "Green")
+            {
+                TxtBlock.Background = Brushes.Green;
+            }
+            else if (colors == "Blue")
+            {
+                TxtBlock.Background = Brushes.Blue;
+            }
+            else if (colors == "Red")
+            {
+                TxtBlock.Background = Brushes.Red;
+            }
+        }
+    }
+}
---
+++ XAML/DataBinding/TextBlockHW/colorConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+
+namespace TextBlockHW
+{
+    class colorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Brush))
+            {
+                return null;
+            }
+            //int age = int.Parse(value.ToString());
+            string color = value.ToString();
+            //int age = int.Parse(value.ToString());
+            if (color=="#ff0000")
+            {
+                return Brushes.Red;
+            }
+            else if (color == "#00ff00")
+            {
+                return Brushes.Green;
+            }
+            else if (color == "#0000ff")
+            {
+                return Brushes.Blue;
+            }
+            else
+            {
+                return Brushes.Bisque;
+            }
+           
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
