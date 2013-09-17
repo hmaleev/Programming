4bfc6fc1c95e53eaa5eb1c94f2ed4103adea8149
Hristo <hmaleev@gmail.com>
2013-09-17 21:02:07 +0300

    Added new project - UnitConvertorApp
    the console application in this project is for testing purpose only. It
    will be removed after several commits

---
+++ Win8 Apps with XAML/UnitConvertor/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance distance = new Distance();
+            float res = distance.Conversion(1, "yard", "m");
+            Console.WriteLine("{0}",res);
+        }
+    }
+}
