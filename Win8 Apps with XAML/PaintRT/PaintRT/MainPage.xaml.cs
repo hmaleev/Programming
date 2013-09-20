af0881cb854d60937aac6de5ea4c85468dee9c91
Hristo <hmaleev@gmail.com>
2013-09-20 17:19:32 +0300

    updated PaintRT

--- Win8 Apps with XAML/PaintRT/PaintRT/MainPage.xaml.cs
+++ Win8 Apps with XAML/PaintRT/PaintRT/MainPage.xaml.cs
@@ -23,13 +23,14 @@ namespace PaintRT
     public sealed partial class MainPage : Page
     {
         RotateTransform rectRotation = new RotateTransform();
-
+        
         Point startPoint = new Point();
         Point endPoint = new Point();
         string currentElement;
-
+        SolidColorBrush color;
         public MainPage()
         {
+         
             this.InitializeComponent();
             rotatingCanvas.RenderTransform = rectRotation;
            
@@ -47,13 +48,35 @@ namespace PaintRT
         private void RotatingCanvas(object sender, ManipulationDeltaRoutedEventArgs e)
         {
             var rect = sender as Canvas;
+         
             rectRotation.CenterX = rect.Width / 2;
             rectRotation.CenterY = rect.Height / 2;
             rectRotation.Angle += e.Delta.Rotation;
+            if (rectRotation.Angle>360)
+            {
+                rectRotation.Angle -= 360;
+            }
+            if ((rectRotation.Angle >= 0 && rectRotation.Angle <= 20) || (rectRotation.Angle >= -20 && rectRotation.Angle <= 0) || (rectRotation.Angle >= 345 && rectRotation.Angle <= 360))
+            {
+                color = new SolidColorBrush(Colors.Red);
+            }
+            else if ((rectRotation.Angle >= 65 && rectRotation.Angle <= 115) )
+            {
+                color = new SolidColorBrush(Colors.Green);
+            }
+            else if ((rectRotation.Angle >= 155 && rectRotation.Angle <= 205))
+            {
+                color = new SolidColorBrush(Colors.Blue);
+            }
+            else if ((rectRotation.Angle >= 245 && rectRotation.Angle <= 295))
+            {
+                color = new SolidColorBrush(Colors.Black);
+            }
         }
 
         private void onTappedLine(object sender, TappedRoutedEventArgs e)
         {
+          
             currentElement = "line";
         }
 
@@ -62,16 +85,11 @@ namespace PaintRT
             GetStartPoint(e);
         }
 
-        private void GetStartPoint(PointerRoutedEventArgs e)
-        {
-            startPoint.X = e.GetCurrentPoint(paintField).Position.X;
-            startPoint.Y = e.GetCurrentPoint(paintField).Position.Y;
-        }
-
+      
         private void onPaintFieldPointerReleased(object sender, PointerRoutedEventArgs e)
         {
             GetEndPoint(e);
-          
+            string a = rectRotation.Angle.ToString();
             switch (currentElement)
             {
                 default:
@@ -84,6 +102,12 @@ namespace PaintRT
             }
         }
 
+        private void GetStartPoint(PointerRoutedEventArgs e)
+        {
+            startPoint.X = e.GetCurrentPoint(paintField).Position.X;
+            startPoint.Y = e.GetCurrentPoint(paintField).Position.Y;
+        }
+
         private void GetEndPoint(PointerRoutedEventArgs e)
         {
             endPoint.X = e.GetCurrentPoint(paintField).Position.X;
@@ -93,7 +117,7 @@ namespace PaintRT
         private Line CreateLine()
         {
             Line line = new Line();
-            line.Stroke = new SolidColorBrush(Colors.Black);
+            line.Stroke = color;
             line.StrokeThickness = 2;
             line.X1 = startPoint.X;
             line.Y1 = startPoint.Y;
