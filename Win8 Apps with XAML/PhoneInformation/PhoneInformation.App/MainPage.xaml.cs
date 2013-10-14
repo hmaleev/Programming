6839c122df497ae185890940bca7261fbc3a0b89
Hristo <hmaleev@gmail.com>
2013-10-14 23:02:37 +0300

    updated PhoneInformation
    fixed snapped view in main page, renamed folder for brand logos

--- Win8 Apps with XAML/PhoneInformation/PhoneInformation.App/MainPage.xaml.cs
+++ Win8 Apps with XAML/PhoneInformation/PhoneInformation.App/MainPage.xaml.cs
@@ -20,7 +20,7 @@ namespace PhoneInformation.App
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : PhoneInformation.App.Common.LayoutAwarePage
     {
         public MainPage()
         {
@@ -38,7 +38,7 @@ namespace PhoneInformation.App
 
         private void onNokiaTap(object sender, TappedRoutedEventArgs e)
         {
-            Parameters.brand = "nokia";
+          Parameters.brand = "nokia";
           this.Frame.Navigate(typeof( Views.Brands.PhoneList));
         }
 
