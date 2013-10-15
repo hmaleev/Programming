0ea5eb6dc897aff0d614a20727f58dc621ac6879
Hristo <hmaleev@gmail.com>
2013-10-16 00:06:53 +0300

    updated PhoneInformation - XAML
    added privacy policy

--- Win8 Apps with XAML/PhoneInformation/PhoneInformation.App/App.xaml.cs
+++ Win8 Apps with XAML/PhoneInformation/PhoneInformation.App/App.xaml.cs
@@ -2,8 +2,11 @@
 using System.Linq;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.UI.ApplicationSettings;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media.Animation;
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
 
@@ -14,15 +17,78 @@ namespace PhoneInformation.App
     /// </summary>
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+
         public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
+
+                void App_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            SettingsCommand command = new SettingsCommand("privacyPolicy", "Privacy policy", (handler) =>
+            {
+                Popup popup = BuildSettingsItem(new Views.Settings.PrivacyPolicy(),346);
+                popup.IsOpen = true;
+            });
+
+            args.Request.ApplicationCommands.Add(command);
+        }
+
+        private Popup BuildSettingsItem(UserControl userControl, int width)
+        {
+            Popup popup = new Popup();
+            popup.IsLightDismissEnabled = true;
+            popup.ChildTransitions = new TransitionCollection();
+            popup.ChildTransitions.Add(new PaneThemeTransition()
+            {
+                Edge = (SettingsPane.Edge == SettingsEdgeLocation.Right) ?
+                        EdgeTransitionLocation.Right :
+                        EdgeTransitionLocation.Left
+            });
+            userControl.Width = width;
+            userControl.Height = Window.Current.Bounds.Height;
+            popup.Child = userControl;
+
+            popup.SetValue(Canvas.LeftProperty, SettingsPane.Edge == SettingsEdgeLocation.Right ? (Window.Current.Bounds.Width - width) : 0);
+            popup.SetValue(Canvas.TopProperty, 0);
+
+            return popup;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //private void SettingCharmManager_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        //{
+        //    args.Request.ApplicationCommands.Add(new SettingsCommand("privacypolicy", "Privacy policy", OpenPrivacyPolicy));
+        //}
+
+
+
+        //private  void OpenPrivacyPolicy(IUICommand command)
+        //{
+        //    //Uri uri = new Uri("http://google.bg");
+        //    //await Windows.System.Launcher.LaunchUriAsync(uri);
+        //SettingsCommand settingsCommand = (SettingsCommand)command;
+        ////((Frame)Window.Current.Content).Navigate(typeof(Views.Settings.PrivacyPolicy), "");
+       
+        //}
 
         /// <summary>
         /// Invoked when the application is launched normally by the end user.  Other entry points
@@ -32,6 +98,9 @@ namespace PhoneInformation.App
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
+
+          //  SettingsPane.GetForCurrentView().CommandsRequested += SettingCharmManager_CommandsRequested;
+            SettingsPane.GetForCurrentView().CommandsRequested += App_CommandsRequested;
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
---
+++ Win8 Apps with XAML/PhoneInformation/PhoneInformation.App/Views/Settings/PrivacyPolicy.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PhoneInformation.App.Views.Settings
+{
+    public sealed partial class PrivacyPolicy : UserControl
+    {
+        public PrivacyPolicy()
+        {
+            this.InitializeComponent();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Popup parent = this.Parent as Popup;
+            if (parent != null)
+            {
+                parent.IsOpen = false;
+            }
+
+            // If the app is not snapped, then the back button shows the Settings pane again.
+            if (Windows.UI.ViewManagement.ApplicationView.Value != Windows.UI.ViewManagement.ApplicationViewState.Snapped)
+            {
+                SettingsPane.Show();
+            }
+        }
+    }
+}
