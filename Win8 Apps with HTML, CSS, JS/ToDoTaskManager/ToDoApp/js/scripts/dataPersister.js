fdd5fc420ac1fe2e222e825dd1cea911b13c1335
Hristo <hmaleev@gmail.com>
2013-10-16 22:58:19 +0300

    updated ToDoTaskManager - WInJS
    added project ToDoTaskManager

---
+++ Win8 Apps with HTML, CSS, JS/ToDoTaskManager/ToDoApp/js/scripts/dataPersister.js
@@ -0,0 +1,82 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+/// <reference path="//Microsoft.WinJS.1.0/js/ui.js" />
+(function () {
+    var userData = {
+        username: "",
+        sessionKey: "",
+        remember: false,
+        data: new Array(),
+        hasChanges: false,
+    };
+
+    var task = {
+        title: "",
+        content: "",
+        finishDate: "",
+        status: ""
+    };
+
+    var createNewTask = function (title, content, finishDate) {
+        var newTask = {
+            title: title,
+            content: content,
+            finishDate: finishDate,
+            status: "images/inprogress.png"
+        };
+
+        userData.hasChanges = true;
+
+        if (userData.data == null) {
+            userData.data = new Array();
+        }
+        userData.data.push(newTask);
+    };
+
+    var ObservableTask = WinJS.Binding.define(task);
+
+    var getObservableTask = function (title, content, finishDate, status) {
+        return new ObservableTask({
+            title: title,
+            content: content,
+            finishDate: finishDate,
+            status: status
+        });
+    }
+
+    var makeBindableTasks = function () {
+
+        var list = new Array();
+
+        for (var i in userData.data) {
+
+            var title = userData.data[i].title;
+            var content = userData.data[i].content;
+            var finishDate = userData.data[i].finishDate;
+            var status = userData.data[i].status;
+
+            list.push(getObservableTask(title, content, finishDate, status));
+        }
+
+        return new WinJS.Binding.List(list);
+    }
+
+    var bindableTasks = new makeBindableTasks();
+
+    var finishTasks = function (args) {
+        for (var i in args) {
+            userData.data[args[i]].status = "images/done.png"
+        }
+    }
+
+    WinJS.Namespace.define("DataPersister", {
+        createNewTask: createNewTask,
+        userData: userData,
+        tasks: bindableTasks,
+
+        update: function () {
+            this.tasks = makeBindableTasks();
+        },
+
+        finishTasks: finishTasks,
+    });
+}())
---
+++ Win8 Apps with HTML, CSS, JS/ToDoTaskManager/ToDoApp/pages/tasks/show/show.js
@@ -0,0 +1,148 @@
+﻿/// <reference path="../../../js/YeahToast.js" />
+/// <reference path="../../../js/scripts/popUpMessage.js" />
+/// <reference path="../../../js/scripts/uiEditor.js" />
+/// <reference path="../../../js/scripts/httpRequester.js" />
+// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    var applicationData = Windows.Storage.ApplicationData.current;
+
+    var localSettings = applicationData.localSettings;
+
+    WinJS.UI.Pages.define("/pages/tasks/show/show.html", {
+        ready: function (element, options) {
+            var appBar = document.getElementById("appbar").winControl;
+            appBar.disabled = false;
+
+            var addButton = document.getElementById("add");
+            var updateButton = document.getElementById("edit");
+            var backButton = document.getElementById("back-button");
+            var syncButton = document.getElementById("sync");
+            var tasksList = document.getElementById("tasks").winControl;
+            var deleteButton = document.getElementById("delete").winControl;
+            var finishButton = document.getElementById("finish").winControl;
+            var logoutButton = document.getElementById("logout").winControl;
+           // var notificationShown = false;
+            var selectedTasks = new Array();
+
+            addButton.addEventListener("click", function () {
+                WinJS.Navigation.navigate("pages/tasks/create/create.html");
+            });
+
+            tasksList.addEventListener("contextmenu", function () {
+                var triger = this.winControl;
+                selectedTasks = new Array();
+
+                triger.selection.getItems().then(function (items) {
+
+                    for (var i in items) {
+                        selectedTasks.push(items[i].index);
+                    }
+                });
+            });
+
+            appBar.addEventListener("beforeshow", function () {
+                if (selectedTasks.length > 1) {
+                    appBar.hideCommands("add");
+                    appBar.hideCommands("edit");
+                    appBar.showCommands("delete");
+                    appBar.showCommands("finish");
+                    if (DataPersister.userData.hasChanges) {
+                        appBar.showCommands("sync");
+                    }
+                    else {
+                        appBar.hideCommands("sync");
+                    }
+                }
+                else if (selectedTasks.length > 0) {
+                    appBar.hideCommands("add");
+                    appBar.showCommands("edit");
+                    appBar.showCommands("delete");
+                    appBar.showCommands("finish");
+                    if (DataPersister.userData.hasChanges) {
+                        appBar.showCommands("sync");
+                    }
+                    else {
+                        appBar.hideCommands("sync");
+                    }
+                }
+                else {
+                    appBar.showCommands("add");
+                    appBar.hideCommands("edit");
+                    appBar.hideCommands("delete");
+                    appBar.hideCommands("finish");
+                    if (DataPersister.userData.hasChanges) {
+                        appBar.showCommands("sync");
+                    }
+                    else {
+                        appBar.hideCommands("sync");
+                    }
+                }
+            });
+
+            deleteButton.addEventListener("click", function () {
+                for (var i = selectedTasks.length-1; i>=0 ; i--) {
+                    DataPersister.userData.data.splice(selectedTasks[i], 1);
+                }
+                selectedTasks = new Array();
+                DataPersister.update();
+                WinJS.Navigation.navigate("/pages/tasks/show/show.html");
+                DataPersister.userData.hasChanges = true;
+            });
+
+            updateButton.addEventListener("click", function () {
+                var taskToEdit = selectedTasks[0];
+                WinJS.Navigation.navigate("/pages/tasks/update/update.html", taskToEdit);
+            });
+
+            finishButton.addEventListener("click", function () {
+                DataPersister.finishTasks(selectedTasks);
+                DataPersister.userData.hasChanges = true;
+                DataPersister.update();
+                WinJS.Navigation.navigate("/pages/tasks/show/show.html");
+            });
+
+            syncButton.addEventListener("click", function () {
+                if (!DataPersister.userData.hasChanges) {
+                    return;
+                }
+
+                var progressBar = new UI.ProgressBar(document.body);
+                progressBar.Show();
+
+                Request.UserSync(DataPersister.userData.sessionKey, JSON.stringify(DataPersister.userData.data)).then(function (request) {
+                   
+                    progressBar.Hide();
+                    //Message.Show("true");\
+                    if (DataPersister.userData.hasChanges) {
+                        YeahToast.show({ title: "Sync done" });
+                        //notificationShown = true;
+                    }
+                    DataPersister.userData.hasChanges = false;
+
+                }, function (error) {
+                    progressBar.Hide();
+                    //Message.Show("false");
+                });
+
+            });
+
+            logoutButton.addEventListener("click", function () {
+                var progressBar = new UI.ProgressBar(document.body);
+                progressBar.Show();
+
+                Request.UserLogOut(DataPersister.userData.sessionKey).then(function (request) {
+                    progressBar.Hide();
+                    DataPersister.userData.remember = false;
+                    localSettings.values["user"] = undefined;
+                    WinJS.Navigation.navigate("pages/menu/login/login.html");
+
+                }, function (request) {
+                    progressBar.Hide();
+                });
+            });
+        }
+    });
+})();
